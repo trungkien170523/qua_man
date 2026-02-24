b7c94e7d1165dfe89da39d4a9b2c8d3f37fce54c
trungkien170523 <trungkien17523@gmail.com>
2026-02-24 21:26:29 +0700

    your message

---
+++ src/main/java/com/codegym/qua_man/QuaManApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.qua_man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuaManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuaManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/qua_man/QuaManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.qua_man;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuaManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
